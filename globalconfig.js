76c44d7dfdf03ec3c0ed48074b79e32c0d5f5cbf
Gregory <gregoryblockchaindev@gmail.com>
2018-12-10 15:06:47 -0800

    test

--- globalconfig.js
+++ globalconfig.js
@@ -1,6 +1,6 @@
 module.exports = {
 	"network": {
-        "dev":{
+        "dev":{  
             "fullNode": "http://127.0.0.1:8090",
             "solidityNode": "http://127.0.0.1:8091",
             "eventServer": "http://127.0.0.1:8092"

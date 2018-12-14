c00b536ad850b8feffffdafc7a7cd17bb221569d
Gurkirat Singh <tablagurkirat@yahoo.com>
2018-12-13 21:04:03 -0800

    precheck views done

---
+++ client/js/TronLinkDownload.js
@@ -0,0 +1,33 @@
+
+
+import React from "react";
+import TronLinkImg from "../images/tron_link.png";
+
+export default class TronLinkDownload extends React.Component{
+	constructor(props){
+		super(props);
+	}
+
+	render(){
+		return(
+			<div className="tronlink_background">
+				<div className="ui center aligned container vertical_center">
+					<p className="tronlink_title m-0">No TronLink</p>
+					<p className="tronlink_subtitle">
+						It seems TronLink is not installed in your browser. TronLink makes connecting with DApps
+						super fast and easy!
+					</p>
+						
+					<a href="https://chrome.google.com/webstore/detail/tronlink/ibnejdfjmmkpcnlpebklmnkoeoihofec"
+						className="ui medium rounded image my-5">
+						<div className="ui red ribbon tiny label">
+							Download Here
+						</div>
+						<img src={TronLinkImg}/>
+					</a>
+				</div>
+			</div>
+		);
+	}
+}
+
